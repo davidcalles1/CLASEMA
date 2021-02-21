fd2cf8a4e9fd2fb59064049e794fa3f49fc5c6c9
nelson calles <cc0324032019@unab.edu.sv>
2021-02-20 21:36:39 -0300

    04
    practica de la semana 04 primera parte

---
+++ Nelson001/Nelson001/VISTA/FormMenu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nelson001.VISTA
+{
+    public partial class FormMenu : Form
+    {
+        public FormMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void clikcToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmAcercaDe frmAcerca = new FrmAcercaDe();
+            frmAcerca.MdiParent = this;
+            frmAcerca.Show();
+        }
+    }
+    
+}
