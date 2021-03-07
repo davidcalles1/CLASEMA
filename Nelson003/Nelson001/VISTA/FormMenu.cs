e10408c94090815ed7b85eeb80015619ac8af7d2
nelson calles <cc0324032019@unab.edu.sv>
2021-03-06 23:14:52 -0300

    actualizacion

---
+++ Nelson003/Nelson001/VISTA/FormMenu.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nelson001.VISTA
+{
+    public partial class FormMenu : Form
+    {
+        public FormMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void clikcToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmAcercaDe frmAcerca = new FrmAcercaDe();
+            frmAcerca.MdiParent = this;
+            frmAcerca.Show();
+        }
+
+        private void cLICKPARAACToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmListaDeusuario frmLista = new FrmListaDeusuario();
+            frmLista.MdiParent = this;
+            frmLista.Show();
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+        public string usuarioEstado;
+        private void FormMenu_Load(object sender, EventArgs e)
+        {
+            label1.Text = usuarioEstado;
+        }
+
+        private void sumasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmOperaciones frmO = new FrmOperaciones();
+            frmO.MdiParent = this;
+            frmO.btnRestaa.Enabled = false;
+
+            frmO.Show();
+
+        }
+
+        private void restaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmOperaciones frmO = new FrmOperaciones();
+            frmO.MdiParent = this;
+            frmO.btnSuma.Enabled = false;
+
+            frmO.Show();
+        }
+    }
+    
+}
---
+++ Nelson003/Nelson001/VISTA/FrmOperaciones.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nelson001.VISTA
+{
+    public partial class FrmOperaciones : Form
+    {
+        public FrmOperaciones()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSuma_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int suma;
+                suma = Convert.ToInt32(txtDato1.Text) + Convert.ToInt32(txtDato2.Text);
+                txtResultado.Text = suma.ToString();
+            } catch (Exception)
+            {
+                MessageBox.Show("tus datos son incorrectos");
+            }
+        }
+
+        private void btnRestaa_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int resta;
+                resta = Convert.ToInt32(txtDato1.Text) - Convert.ToInt32(txtDato2.Text);
+                txtResultado.Text = resta.ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("tus datos son incorrectos");
+            }
+
+        }
+    }
+}
